34bf9c318fd7c196e7fd5c39970d2e9069075c80
Leo Bishop <leobishop@Leos-MacBook-Pro.local>
2024-05-06 16:53:04 +0100

    moving into src directory

---
+++ src/core/network/components/network_interface_test.go
@@ -0,0 +1,382 @@
+package components
+
+import (
+	"testing"
+
+	"main/src/domain"
+	"main/src/traffic/packet"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewNetworkInterface(t *testing.T) {
+	t.Parallel()
+
+	t.Run("ImplementsInterface", func(t *testing.T) {
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 8, 1)
+		require.NoError(t, err)
+
+		assert.Implements(t, (*NetworkInterface)(nil), netIntfc)
+	})
+
+	t.Run("Valid", func(t *testing.T) {
+		var nodeID domain.NodeID = domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}
+		var bufferSize int = 1
+		var flitSize int = 8
+		var maxPriority int = 1
+
+		netIntfc, err := newNetworkInterface(nodeID, bufferSize, flitSize, maxPriority)
+		require.NoError(t, err)
+
+		assert.NotNil(t, netIntfc)
+		assert.Equal(t, nodeID, netIntfc.NodeID())
+		assert.Equal(t, bufferSize, netIntfc.bufferSize)
+		assert.Equal(t, flitSize, netIntfc.flitSize)
+		assert.Equal(t, maxPriority, netIntfc.maxPriority)
+		assert.NotNil(t, netIntfc.pendingPackets)
+		assert.NotNil(t, netIntfc.flitsInTransit)
+		assert.NotNil(t, netIntfc.flitsArriving)
+		assert.NotNil(t, netIntfc.arrivedPackets)
+	})
+
+	t.Run("InvalidBufferSize", func(t *testing.T) {
+		var flitSize int = 8
+
+		_, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 0, flitSize, 1)
+		require.Error(t, err)
+	})
+}
+
+func TestNetworkInterfaceNodeID(t *testing.T) {
+	t.Parallel()
+
+	var nodeID domain.NodeID = domain.NodeID{ID: "n", Pos: domain.NewPosition(0, 0)}
+
+	netIntfc, err := newNetworkInterface(nodeID, 1, 8, 1)
+	require.NoError(t, err)
+
+	assert.Equal(t, nodeID, netIntfc.NodeID())
+}
+
+func TestNetworkInterfaceSetInputPort(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid", func(t *testing.T) {
+		var bufferSize int = 1
+		var maxPriority int = 1
+
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, bufferSize, 8, maxPriority)
+		require.NoError(t, err)
+
+		conn, err := NewConnection(maxPriority)
+		require.NoError(t, err)
+
+		err = netIntfc.SetInputPort(conn)
+		require.NoError(t, err)
+
+		assert.Equal(t, conn, netIntfc.inputPort.connection())
+		assert.Equal(t, netIntfc.NodeID(), netIntfc.inputPort.connection().GetDstRouter())
+	})
+
+	t.Run("NilConnection", func(t *testing.T) {
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 8, 1)
+		require.NoError(t, err)
+
+		err = netIntfc.SetInputPort(nil)
+		require.ErrorIs(t, err, domain.ErrNilParameter)
+	})
+
+	t.Run("InvalidBufferSize", func(t *testing.T) {
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 8, 1)
+		require.NoError(t, err)
+
+		netIntfc.bufferSize = 0
+
+		err = netIntfc.SetInputPort(&connectionImpl{})
+		require.Error(t, err)
+	})
+}
+
+func TestNetworkInterfaceSetOutputPort(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid", func(t *testing.T) {
+		var bufferSize int = 1
+		var maxPriority int = 1
+
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, bufferSize, 8, maxPriority)
+		require.NoError(t, err)
+
+		conn, err := NewConnection(maxPriority)
+		require.NoError(t, err)
+
+		err = netIntfc.SetOutputPort(conn)
+		require.NoError(t, err)
+
+		assert.Equal(t, conn, netIntfc.outputPort.connection())
+		assert.Equal(t, netIntfc.NodeID(), netIntfc.outputPort.connection().GetSrcRouter())
+	})
+
+	t.Run("NilConnection", func(t *testing.T) {
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 8, 1)
+		require.NoError(t, err)
+
+		err = netIntfc.SetOutputPort(nil)
+		require.ErrorIs(t, err, domain.ErrNilParameter)
+	})
+}
+
+func TestNetworkInterfaceRoutePacket(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid", func(t *testing.T) {
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 8, 1)
+		require.NoError(t, err)
+
+		var src domain.NodeID = domain.NodeID{ID: "n1", Pos: domain.NewPosition(0, 0)}
+		var dst domain.NodeID = domain.NodeID{ID: "n2", Pos: domain.NewPosition(0, 1)}
+		var route domain.Route = domain.Route{src, dst}
+
+		pkt := packet.NewPacket("t", 1, 100, route, 4)
+
+		err = netIntfc.RoutePacket(pkt)
+		require.NoError(t, err)
+		assert.Contains(t, netIntfc.pendingPackets[pkt.Priority()], pkt)
+	})
+
+	t.Run("NilPacket", func(t *testing.T) {
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 8, 1)
+		require.NoError(t, err)
+
+		err = netIntfc.RoutePacket(nil)
+		require.ErrorIs(t, err, domain.ErrNilParameter)
+	})
+}
+
+func TestNetworkInterfacePopArrivedPackets(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid", func(t *testing.T) {
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 8, 1)
+		require.NoError(t, err)
+
+		var src domain.NodeID = domain.NodeID{ID: "n1", Pos: domain.NewPosition(0, 0)}
+		var dst domain.NodeID = domain.NodeID{ID: "n2", Pos: domain.NewPosition(0, 1)}
+		var route domain.Route = domain.Route{src, dst}
+
+		pkts := []packet.Packet{
+			packet.NewPacket("t", 1, 100, route, 4),
+			packet.NewPacket("t", 1, 100, route, 4),
+			packet.NewPacket("t", 1, 100, route, 4),
+		}
+		netIntfc.arrivedPackets = append(netIntfc.arrivedPackets, pkts...)
+
+		gotPkts := netIntfc.PopArrivedPackets()
+		assert.Equal(t, pkts, gotPkts)
+		assert.Empty(t, netIntfc.arrivedPackets)
+	})
+}
+
+func TestNetworkInterfaceHandleArrivingFlits(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid", func(t *testing.T) {
+		var src domain.NodeID = domain.NodeID{ID: "n1", Pos: domain.NewPosition(0, 0)}
+		var dst domain.NodeID = domain.NodeID{ID: "n2", Pos: domain.NewPosition(0, 1)}
+		var route domain.Route = domain.Route{src, dst}
+		var bufferSize int = 1
+		var flitSize int = 2
+		var maxPriority int = 1
+
+		netIntfc, err := newNetworkInterface(src, bufferSize, flitSize, maxPriority)
+		require.NoError(t, err)
+
+		pkt := packet.NewPacket("t", 1, 100, route, 4)
+		flits := pkt.Flits(flitSize)
+
+		inConn, err := NewConnection(maxPriority)
+		require.NoError(t, err)
+
+		err = netIntfc.SetInputPort(inConn)
+		require.NoError(t, err)
+
+		for i := 0; i <= maxPriority; i++ {
+			<-inConn.creditChannel(i)
+		}
+
+		for i := 0; i < len(flits); i++ {
+			inConn.flitChannel() <- flits[i]
+
+			err = netIntfc.HandleArrivingFlits()
+			require.NoError(t, err)
+
+			<-inConn.creditChannel(flits[i].Priority())
+		}
+
+		require.NoError(t, packet.EqualPackets(pkt, netIntfc.arrivedPackets[0]))
+	})
+}
+
+func TestNetworkInterfaceArrivedHeaderFlit(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid", func(t *testing.T) {
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 1, 1)
+		require.NoError(t, err)
+
+		headerFlit := packet.NewHeaderFlit("t", uuid.New(), 1, 100, domain.Route{domain.NodeID{ID: "n1", Pos: domain.NewPosition(0, 0)}, domain.NodeID{ID: "n2", Pos: domain.NewPosition(0, 1)}})
+
+		err = netIntfc.arrivedHeaderFlit(headerFlit)
+		require.NoError(t, err)
+
+		assert.Contains(t, netIntfc.flitsArriving, headerFlit.PacketUUID())
+	})
+
+	t.Run("SetHeaderError", func(t *testing.T) {
+		t.Skip("Cannot currently test, possible error cases cannot be met")
+	})
+}
+
+func TestNetworkInterfaceArrivedBodyFlit(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid", func(t *testing.T) {
+		var pktUUID uuid.UUID = uuid.New()
+
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 1, 1)
+		require.NoError(t, err)
+
+		netIntfc.flitsArriving[pktUUID] = packet.NewReconstructor()
+
+		bodyFlit := packet.NewBodyFlit(pktUUID, 4, 1)
+
+		err = netIntfc.arrivedBodyFlit(bodyFlit)
+		require.NoError(t, err)
+	})
+
+	t.Run("AddBodyError", func(t *testing.T) {
+		t.Skip("Cannot currently test, possible error cases cannot be met")
+	})
+}
+
+func TestNetworkInterfaceArrivedTailFlit(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Valid", func(t *testing.T) {
+		var trafficFlowID string = "t"
+		var pktUUID uuid.UUID = uuid.New()
+		var priority int = 1
+		var deadline int = 100
+		var src domain.NodeID = domain.NodeID{ID: "n1", Pos: domain.NewPosition(0, 0)}
+		var dst domain.NodeID = domain.NodeID{ID: "n2", Pos: domain.NewPosition(0, 1)}
+		var route domain.Route = domain.Route{src, dst}
+		var bodySize int = 1
+
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 1, 1)
+		require.NoError(t, err)
+
+		headerFlit := packet.NewHeaderFlit(trafficFlowID, pktUUID, priority, deadline, route)
+		netIntfc.flitsArriving[pktUUID] = packet.NewReconstructor()
+		err = netIntfc.flitsArriving[pktUUID].SetHeader(headerFlit)
+		require.NoError(t, err)
+
+		bodyFlit := packet.NewBodyFlit(pktUUID, bodySize, priority)
+		err = netIntfc.flitsArriving[pktUUID].AddBody(bodyFlit)
+		require.NoError(t, err)
+
+		tailFlit := packet.NewTailFlit(pktUUID, priority)
+		err = netIntfc.arrivedTailFlit(tailFlit)
+		require.NoError(t, err)
+
+		assert.Equal(t, pktUUID, netIntfc.arrivedPackets[0].UUID())
+		assert.Equal(t, trafficFlowID, netIntfc.arrivedPackets[0].TrafficFlowID())
+		assert.Equal(t, priority, netIntfc.arrivedPackets[0].Priority())
+		assert.Equal(t, deadline, netIntfc.arrivedPackets[0].Deadline())
+		assert.Equal(t, route, netIntfc.arrivedPackets[0].Route())
+		assert.Equal(t, bodySize, netIntfc.arrivedPackets[0].BodySize())
+
+		assert.NotContains(t, netIntfc.flitsArriving, pktUUID)
+	})
+
+	t.Run("SetTailError", func(t *testing.T) {
+		var pktUUID uuid.UUID = uuid.New()
+
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 1, 1)
+		require.NoError(t, err)
+
+		netIntfc.flitsArriving[pktUUID] = packet.NewReconstructor()
+		netIntfc.flitsArriving[pktUUID].SetTail(packet.NewTailFlit(pktUUID, 1))
+
+		tailFlit := packet.NewTailFlit(pktUUID, 1)
+		err = netIntfc.arrivedTailFlit(tailFlit)
+		require.Error(t, err)
+	})
+
+	t.Run("ReconstructError", func(t *testing.T) {
+		var pktUUID uuid.UUID = uuid.New()
+
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 1, 1)
+		require.NoError(t, err)
+
+		netIntfc.flitsArriving[pktUUID] = packet.NewReconstructor()
+
+		tailFlit := packet.NewTailFlit(pktUUID, 1)
+		err = netIntfc.arrivedTailFlit(tailFlit)
+		require.Error(t, err)
+	})
+}
+
+func TestNetworkInterfaceTransmitPendingPackets(t *testing.T) {
+	t.Parallel()
+
+	t.Run("NoFlitsToTransit", func(t *testing.T) {
+		var maxPriority int = 1
+
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, 1, 1, maxPriority)
+		require.NoError(t, err)
+
+		conn, err := NewConnection(maxPriority)
+		require.NoError(t, err)
+
+		err = netIntfc.SetOutputPort(conn)
+		require.NoError(t, err)
+
+		err = netIntfc.TransmitPendingPackets()
+		require.NoError(t, err)
+	})
+
+	t.Run("SentFlit", func(t *testing.T) {
+		var src domain.NodeID = domain.NodeID{ID: "n1", Pos: domain.NewPosition(0, 0)}
+		var dst domain.NodeID = domain.NodeID{ID: "n2", Pos: domain.NewPosition(0, 1)}
+		var route domain.Route = domain.Route{src, dst}
+		var bufferSize int = 1
+		var maxPriority int = 1
+
+		netIntfc, err := newNetworkInterface(domain.NodeID{ID: "i", Pos: domain.NewPosition(0, 0)}, bufferSize, 1, maxPriority)
+		require.NoError(t, err)
+
+		conn, err := NewConnection(maxPriority)
+		require.NoError(t, err)
+
+		buff, err := newBuffer(bufferSize, maxPriority)
+		require.NoError(t, err)
+		newInputPort(conn, buff)
+
+		err = netIntfc.SetOutputPort(conn)
+		require.NoError(t, err)
+
+		pkt := packet.NewPacket("t", 1, 100, route, 4)
+
+		err = netIntfc.RoutePacket(pkt)
+		require.NoError(t, err)
+
+		err = netIntfc.TransmitPendingPackets()
+		require.NoError(t, err)
+
+		gotFlit := <-conn.flitChan
+		assert.Equal(t, pkt.Flits(1)[0].PacketUUID(), gotFlit.PacketUUID())
+		assert.Equal(t, pkt.Flits(1)[0].Type(), gotFlit.Type())
+	})
+}
---
+++ src/core/network/network.go
@@ -0,0 +1,250 @@
+package network
+
+import (
+	"main/log"
+	"main/src/core/network/components"
+	"main/src/domain"
+	"main/src/topology"
+)
+
+type Network interface {
+	NetworkInterfaces() []components.NetworkInterface
+	Routers() []components.Router
+
+	NetworkInterfaceMap() map[domain.NodeID]components.NetworkInterface
+	RouterMap() map[domain.NodeID]components.Router
+
+	NetworkInterfacesIDMap() map[string]components.NetworkInterface
+	RoutersIDMap() map[string]components.Router
+
+	Topology() *topology.Topology
+
+	Cycle() error
+}
+
+type networkImpl struct {
+	netwrkIntfcs []components.NetworkInterface
+	routers      []components.Router
+
+	netwrkIntfcMap map[domain.NodeID]components.NetworkInterface
+	routerMap      map[domain.NodeID]components.Router
+
+	netwrkIntfcIDMap map[string]components.NetworkInterface
+	routerIDMap      map[string]components.Router
+
+	top *topology.Topology
+}
+
+func NewNetwork(top *topology.Topology, conf domain.SimConfig) (Network, error) {
+	routerNodes, err := buildNetwork(top, conf)
+	if err != nil {
+		log.Log.Error().Err(err).Msg("error building network")
+		return nil, err
+	}
+
+	netwrkIntfcs := make([]components.NetworkInterface, len(routerNodes))
+	index := 0
+	for id := range routerNodes {
+		netwrkIntfcs[index] = routerNodes[id].NetworkInterface
+		index++
+	}
+
+	routers := make([]components.Router, len(routerNodes))
+	index = 0
+	for id := range routerNodes {
+		routers[index] = routerNodes[id].Router
+		index++
+	}
+
+	netwrkIntfcMap := make(map[domain.NodeID]components.NetworkInterface)
+	for id := range routerNodes {
+		netwrkIntfcMap[routerNodes[id].NetworkInterface.NodeID()] = routerNodes[id].NetworkInterface
+	}
+
+	routerMap := make(map[domain.NodeID]components.Router)
+	for id := range routerNodes {
+		routerMap[routerNodes[id].Router.NodeID()] = routerNodes[id].Router
+	}
+
+	netwrkIntfcIDMap := make(map[string]components.NetworkInterface)
+	for id := range routerNodes {
+		netwrkIntfcIDMap[routerNodes[id].NetworkInterface.NodeID().ID] = routerNodes[id].NetworkInterface
+	}
+
+	routerIDMap := make(map[string]components.Router)
+	for id := range routerNodes {
+		routerIDMap[routerNodes[id].Router.NodeID().ID] = routerNodes[id].Router
+	}
+
+	return &networkImpl{
+		netwrkIntfcs: netwrkIntfcs,
+		routers:      routers,
+
+		netwrkIntfcMap: netwrkIntfcMap,
+		routerMap:      routerMap,
+
+		netwrkIntfcIDMap: netwrkIntfcIDMap,
+		routerIDMap:      routerIDMap,
+
+		top: top,
+	}, nil
+}
+
+func buildNetwork(top *topology.Topology, conf domain.SimConfig) (map[string]components.RouterNode, error) {
+	log.Log.Debug().Msg("constructing network from topology")
+
+	routerNodes := make(map[string]components.RouterNode)
+
+	log.Log.Debug().Msg("creating routers")
+	for id := range top.Nodes() {
+		node, exists := top.Node(id)
+		if !exists {
+			log.Log.Error().Err(domain.ErrInvalidTopology).Str("id", id).Msg("node does not exist")
+			return nil, domain.ErrInvalidTopology
+		}
+
+		rNode, err := components.NewRouterNode(components.RouterConfig{
+			NodeID:    node.NodeID(),
+			SimConfig: conf,
+		})
+		if err != nil {
+			log.Log.Error().Err(err).Str("id", node.NodeID().ID).Msg("error creating router")
+			return nil, err
+		}
+
+		routerNodes[rNode.NodeID().ID] = rNode
+	}
+
+	log.Log.Debug().Msg("connecting routers")
+	for id := range top.Edges() {
+		edge, exists := top.Edge(id)
+		if !exists {
+			log.Log.Error().Err(domain.ErrInvalidTopology).Str("id", id).Msg("edge does not exist")
+			return nil, domain.ErrInvalidTopology
+		}
+
+		aRouterNode, exists := routerNodes[edge.A()]
+		if !exists {
+			log.Log.Error().Err(domain.ErrInvalidTopology).Str("edge_id", edge.ID()).Str("router_id", edge.A()).Msg("edge A router does not exist")
+			return nil, domain.ErrInvalidTopology
+		}
+
+		bRouterNode, exists := routerNodes[edge.B()]
+		if !exists {
+			log.Log.Error().Err(domain.ErrInvalidTopology).Str("edge_id", edge.ID()).Str("router_id", edge.B()).Msg("edge B router does not exist")
+			return nil, domain.ErrInvalidTopology
+		}
+
+		aToB, err := components.NewConnection(conf.MaxPriority)
+		if err != nil {
+			log.Log.Error().Err(err).Str("id", edge.ID()).Msg("error creating connection")
+			return nil, err
+		}
+		if err := aRouterNode.Router.RegisterOutputPort(aToB); err != nil {
+			log.Log.Error().Err(err).Str("edge_id", edge.ID()).Str("router_id", aRouterNode.NodeID().ID).Msg("error registering output port")
+			return nil, err
+		}
+		if err := bRouterNode.Router.RegisterInputPort(aToB); err != nil {
+			log.Log.Error().Err(err).Str("edge_id", edge.ID()).Str("router_id", bRouterNode.NodeID().ID).Msg("error registering input port")
+			return nil, err
+		}
+
+		bToA, err := components.NewConnection(conf.MaxPriority)
+		if err != nil {
+			log.Log.Error().Err(err).Str("id", edge.ID()).Msg("error creating connection")
+			return nil, err
+		}
+		if err := bRouterNode.Router.RegisterOutputPort(bToA); err != nil {
+			log.Log.Error().Err(err).Str("edge_id", edge.ID()).Str("router_id", bRouterNode.NodeID().ID).Msg("error registering output port")
+			return nil, err
+		}
+		if err := aRouterNode.Router.RegisterInputPort(bToA); err != nil {
+			log.Log.Error().Err(err).Str("edge_id", edge.ID()).Str("router_id", aRouterNode.NodeID().ID).Msg("error registering input port")
+			return nil, err
+		}
+	}
+
+	log.Log.Info().Msg("build network from topology")
+	return routerNodes, nil
+}
+
+func (n *networkImpl) NetworkInterfaces() []components.NetworkInterface {
+	return n.netwrkIntfcs
+}
+
+func (n *networkImpl) Routers() []components.Router {
+	return n.routers
+}
+
+func (n *networkImpl) NetworkInterfaceMap() map[domain.NodeID]components.NetworkInterface {
+	return n.netwrkIntfcMap
+}
+
+func (n *networkImpl) RouterMap() map[domain.NodeID]components.Router {
+	return n.routerMap
+}
+
+func (n *networkImpl) NetworkInterfacesIDMap() map[string]components.NetworkInterface {
+	return n.netwrkIntfcIDMap
+}
+
+func (n *networkImpl) RoutersIDMap() map[string]components.Router {
+	return n.routerIDMap
+}
+
+func (n *networkImpl) Cycle() error {
+	for i := 0; i < len(n.netwrkIntfcs); i++ {
+		if err := n.netwrkIntfcs[i].TransmitPendingPackets(); err != nil {
+			log.Log.Error().Err(err).Str("id", n.netwrkIntfcs[i].NodeID().ID).Msg("error transmitting network interface's pending packets")
+			return err
+		}
+	}
+
+	if err := n.cycleRouters(); err != nil {
+		log.Log.Error().Err(err).Msg("error cycling routers")
+		return err
+	}
+
+	for i := 0; i < len(n.netwrkIntfcs); i++ {
+		if err := n.netwrkIntfcs[i].HandleArrivingFlits(); err != nil {
+			log.Log.Error().Err(err).Str("id", n.netwrkIntfcs[i].NodeID().ID).Msg("error handling network interface's arriving flits")
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (n *networkImpl) Topology() *topology.Topology {
+	return n.top
+}
+
+func (n *networkImpl) cycleRouters() error {
+	for i := 0; i < len(n.routers); i++ {
+		n.routers[i].UpdateOutputMap()
+	}
+
+	for i := 0; i < len(n.routers); i++ {
+		if err := n.routers[i].UpdateOutputPortsCredit(); err != nil {
+			log.Log.Error().Err(err).Str("id", n.routers[i].NodeID().ID).Msg("error updating router output ports credit")
+			return err
+		}
+	}
+
+	for i := 0; i < len(n.routers); i++ {
+		if err := n.routers[i].RouteBufferedFlits(); err != nil {
+			log.Log.Error().Err(err).Str("id", n.routers[i].NodeID().ID).Msg("error routing buffered flits")
+			return err
+		}
+	}
+
+	for i := 0; i < len(n.routers); i++ {
+		if err := n.routers[i].ReadFromInputPorts(); err != nil {
+			log.Log.Error().Err(err).Str("id", n.routers[i].NodeID().ID).Msg("error reading from router input ports")
+			return err
+		}
+	}
+
+	log.Log.Trace().Msg("performed network cycle for network routers")
+	return nil
+}
