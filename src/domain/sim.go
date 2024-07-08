e712d1f30ff4c16cee7db7c8bf18c873819756b5
Leo Bishop <leobishop@Leos-MacBook-Pro.local>
2024-07-08 23:20:43 +0100

    removed flit size from config

--- src/domain/sim.go
+++ src/domain/sim.go
@@ -3,7 +3,6 @@ package domain
 type SimConfig struct {
 	CycleLimit      int `yaml:"cycle_limit" json:"cycle_limit"`
 	MaxPriority     int `yaml:"max_priority" json:"max_priority"`
-	FlitSize        int `yaml:"flit_size" json:"flit_size"`
 	BufferSize      int `yaml:"buffer_size" json:"buffer_size"`
 	LinkBandwidth   int `yaml:"link_bandwidth" json:"link_bandwidth"`
 	ProcessingDelay int `yaml:"processing_delay" json:"processing_delay"`
